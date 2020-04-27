28a85c726390b833d603ead479f0631144ff67f9
SushmithaPriyanka <sushmitha7908@gmail.com>
2020-04-27 13:38:20 +0530

    Changes made in system

--- PrintMessage.java
+++ PrintMessage.java
@@ -4,6 +4,7 @@ public class PrintMessage{
     public static void main(String args[]){
         String msg= "Welcome to object oriented programming";
         System.out.println(msg);
+        System.out.println(Sushmitha);
     }
 }
 //this is a github demo
